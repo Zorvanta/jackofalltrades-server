8698a9d0dc3c2bb9ea384eaa7f110028000c9185
Austin Vandegriff <austinvandegriff@yahoo.com>
2026-03-10 16:56:06 -0400

    validate required fields email, username, password

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,4 +1,13 @@
 //controller function for creating a user
 export async function createUser(req,res){
-res.json({ message: "Create user controller hit"});
+const { email, username, password } = req.body;
+	if(!email || !username || !password){
+		return res.status(400).json({
+			 error: "email, username, and password are required"
+			});
+	}else{
+		return res.status(200).json({
+			fields: "email, username, and password were sent"
+		});
+	}
 }
