0a191f91d161b84d7f86c68a3a5fe6dba33e5653
Austin Vandegriff <austinvandegriff@yahoo.com>
2026-04-03 16:59:19 -0400

    Check if email is unique

--- backend/src/queries/users.queries.js
+++ backend/src/queries/users.queries.js
@@ -1,10 +1,10 @@
 import pool from "../db/db.js";
 
 export async function findUserByUsername(username) {
-const result = await pool.query(
-	"SELECT id, email, username, password_hash, created_at FROM users WHERE username = $1", [username]
-	);
-	return result;
+	const result = await pool.query(
+		"SELECT id, email, username, password_hash, created_at FROM users WHERE username = $1", [username]
+		);
+		return result;
 }
 export async function createUserQuery(email, username, passwordHash) {
 	const result = await pool.query(
@@ -12,6 +12,13 @@ export async function createUserQuery(email, username, passwordHash) {
 		 VALUES ($1, $2, $3)
 		 RETURNING id, email, username, created_at`,
 		 [email, username, passwordHash]
+	);
+	return result;
+
+}
+export async function findEmailbyEmail(email) {
+	const result = await pool.query(
+		"SELECT email FROM users WHERE email = $1", [email]
 	);
 	return result;
 
