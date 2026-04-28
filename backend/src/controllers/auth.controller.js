ef7e1fb0fe560482f20ac5720583bfc58359bca4
A-Vandegriff <austinvandegriff@yahoo.com>
2026-04-28 14:29:11 -0400

    Implement POST /auth/login endpoint, ensure username and password were provided

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,13 @@
 
-export async function userLogin(){
-console.log('routing successful');
-
+export async function userLogin(req, res){
+	const { username, password } = req.body;
+	if(!username || !password){
+		return res.status(400).send({
+		message : "provide username and password"
+		});
+	}
+	
+	res.status(200).send({
+		end : "end of stream"
+	});
 }
